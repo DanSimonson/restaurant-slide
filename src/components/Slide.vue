74af429bac851a651185da51d857fbdf58add85a
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2018-10-08 09:24:59 +0800

    added navigation features

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class='carousel-view'>
+  <div class='carousel-view flex1'>
+    <div class='button-prev flex2' @click="previous"></div>
     <transition-group class='carousel' tag="div">
       <div v-for="(slide, index) in slides" class='slide' :key="slide.id" :style="{backgroundImage: 'url(' + slides[index].imgLink + ')'}">
         <!--<img :src='slide.imgLink'>-->
         <h4> {{ slide.title }} </h4>
       </div>
     </transition-group>
-    <div class='carousel-controls'>
+    <div class='button-next flex2' @click="next"></div>
+    <!--<div class='carousel-controls'>
       <button class='carousel-controls__button' @click="previous">prev</button>
       <button class='carousel-controls__button' @click="next">next</button>
-    </div>
+    </div>-->
   </div>
 </template>
 
@@ -48,6 +50,13 @@
         ]
       }
     },
+    created() {
+      let selfie = this;
+      setInterval(() => {
+        selfie.next();
+      }, 4000)
+
+    },
     methods: {
       next() {
         const first = this.slides.shift()
@@ -67,12 +76,51 @@
     color: #fff;
   }
 
+  .flex1 {
+    flex: 1 1 20em;
+  }
+
+  .flex2 {
+    flex: 2 2 20em;
+  }
+
+  .carousel-view {
+    position: relative;
+  }
+
+  .carousel-view .button-prev,
+  .carousel-view .button-next {
+    /*height: 20px;
+    width: 20px;
+    background-color: yellow;*/
+    position: absolute;
+    top: 50%;
+    border: 12px solid transparent;
+    /*border-left-color: transparent;
+    border-right-color: purple;
+    border-bottom-color: violet;
+    border-top-color: crimson;
+    */
+  }
+
+  .carousel-view .button-prev {
+    border-right-color: black;
+    transform: rotate(-3deg)
+  }
+
+
+  .carousel-view .button-next {
+    right: 63.5%;
+    border-left-color: black;
+    transform: rotate(3deg);
+    margin-left: 2px;
+  }
+
   .carousel {
     display: flex;
     justify-content: center;
     align-items: center;
     overflow: hidden;
-
     width: 24em;
     min-height: 25em;
   }
@@ -84,7 +132,7 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    border: 0.1em dashed #000;
+    border: 0.1em solid #000;
     border-radius: 50%;
     transition: transform 0.3s ease-in-out;
   }
@@ -96,6 +144,4 @@
   .slide:last-of-type {
     opacity: 0;
   }
-</style>
-
 </style>
